b9394316fd96437b87068dfd0a6ed313be5ca66d
Kenneth Lowe <kennethlowe45@yahoo.COM>
2025-05-23 19:58:08 -0500

    note removed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Replace "hello-vite" with the actual repo name
 export default defineConfig({
   base: "/news-letter/",
   plugins: [react()],
